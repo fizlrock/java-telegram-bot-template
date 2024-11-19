91bb97e7f3ebf6435d434d8246e1910219de27f8
fizlrock <nirku0soft@yandex.ru>
2024-11-19 13:57:21 +0400

    echo bot

--- app/src/main/java/org/example/TelegramBot.java
+++ app/src/main/java/org/example/TelegramBot.java
@@ -1,28 +1,37 @@
 package org.example;
 
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 /**
  * TelegramBot
  */
 public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
-   // private TelegramClient telegramClient = new OkHttpTelegramClient("12345:YOUR_TOKEN");
+
+   String botToken = "7849739712:AAGUU_05X4wX91mjYop8NRMRDpPP5pINn5A";
+   
+   TelegramClient tgClient = new OkHttpTelegramClient(botToken);
 
    @Override
    public void consume(Update update) {
-      System.out.println(update);
-      // We check if the update has a message and the message has text
-      // if (update.hasMessage() && update.getMessage().hasText()) {
-      //     // Create your send message object
-      //     SendMessage sendMessage = new SendMessage(update.getMessage().getChatId(), update.getMessage().getText());
-      //     try {
-      //        // Execute it
-      //        telegramClient.execute(method);
-      //     } catch (TelegramApiException e) {
-      //        e.printStackTrace();
-      //     }
-      // }
+
+      if (update.hasMessage() && update.getMessage().hasText()) {
+          // Create your send message object
+          // SendMessage sendMessage = new SendMessage(update.getMessage().getChatId(), update.getMessage().getText());
+
+          SendMessage sendMessage = new SendMessage(update.getMessage().getChatId().toString(), update.getMessage().getText());
+
+          try {
+             // Execute it
+             tgClient.execute(sendMessage);
+          } catch (TelegramApiException e) {
+             e.printStackTrace();
+          }
+      }
    }
 }
 
